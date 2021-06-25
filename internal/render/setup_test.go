cb2355112604f4b1a8822581fc6de7b7f356cc7d
gdguesser <gabrieldguesser@hotmail.com>
2021-06-24 21:42:34 -0300

    finishing handlers tests and start writing render tests

---
+++ internal/render/setup_test.go
@@ -0,0 +1,10 @@
+package render
+
+import (
+	"github.com/alexedwards/scs/v2"
+	"github.com/gdguesser/gobookings/internal/config"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
