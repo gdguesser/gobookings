966f781c3ae98bb08943e9220efb552dde798f5a
gdguesser <gabrieldguesser@hotmail.com>
2021-06-29 17:57:39 -0300

    improving error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,10 +6,12 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/gdguesser/gobookings/internal/config"
 	"github.com/gdguesser/gobookings/internal/handlers"
+	"github.com/gdguesser/gobookings/internal/helpers"
 	"github.com/gdguesser/gobookings/internal/models"
 	"github.com/gdguesser/gobookings/internal/render"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -17,6 +19,8 @@ const portNumber = ":8083"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main application function
 func main() {
@@ -43,6 +47,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -61,10 +71,9 @@ func run() error {
 	app.UseCache = false
 
 	repo := handlers.NewRepo(&app)
-
 	handlers.NewHandlers(repo)
-
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
