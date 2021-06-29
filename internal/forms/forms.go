e7133ca3fce9c94d9e8178ecd66a40e359f87e04
gdguesser <gabrieldguesser@hotmail.com>
2021-06-29 02:20:28 -0300

    fixing and finishing form tests

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -39,7 +39,7 @@ func (f *Form) Required(fields ...string) {
 
 // Has checks if form field is in post and not empty
 func (f *Form) Has(field string, r *http.Request) bool {
-	x := r.Form.Get(field)
+	x := f.Get(field)
 
 	if x == "" {
 		return false
@@ -49,7 +49,7 @@ func (f *Form) Has(field string, r *http.Request) bool {
 
 // MinLength checks for string minimum length
 func (f *Form) MinLength(field string, length int, r *http.Request) bool {
-	x := r.Form.Get(field)
+	x := f.Get(field)
 
 	if len(x) < length {
 		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
