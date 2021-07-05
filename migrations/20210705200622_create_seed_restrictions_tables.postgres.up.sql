6c07ebffabb5f4e74bc5d36c0de527f208bd8c86
gdguesser <gdguesser@gmail.com>
2021-07-05 17:07:11 -0300

    adding migrations

---
+++ migrations/20210705200622_create_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+('Reservation','2021-07-05 09:58:09.000','2021-07-05 09:58:09.000'),
+('Owner Block','2021-07-05 09:58:09.000','2021-07-05 09:58:09.000');
